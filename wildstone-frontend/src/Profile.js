a640dc3bab7506defa14b89c0aab2e0a3cefa107
Brady Newhard <brady.russell@gmail.com>
2025-05-18 23:50:56 -0400

    Netlify Fail Fix

--- wildstone-frontend/src/Profile.js
+++ wildstone-frontend/src/Profile.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Box, Typography, Container, Grid, Card, CardContent, List, ListItem, ListItemText, Link, Divider, Chip, Avatar } from '@mui/material';
-import EmailIcon from '@mui/icons-material/Email';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 const skills = [
   { label: 'Programming Languages', value: 'JavaScript, Python, HTML, CSS' },
